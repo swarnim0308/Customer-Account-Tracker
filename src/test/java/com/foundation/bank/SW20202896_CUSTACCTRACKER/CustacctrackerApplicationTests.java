a337148c4ff8085eee03050054922adee30f1c78
Swarnim Shukla <swarnim0300@gmail.com>
2024-11-26 17:48:14 +0530

    fix test

--- src/test/java/com/foundation/bank/SW20202896_CUSTACCTRACKER/CustacctrackerApplicationTests.java
+++ src/test/java/com/foundation/bank/SW20202896_CUSTACCTRACKER/CustacctrackerApplicationTests.java
@@ -1,9 +1,10 @@
 package com.foundation.bank.SW20202896_CUSTACCTRACKER;
 
+import com.bank.custaccttracker.CustacctrackerApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CustacctrackerApplication.class)
 class CustacctrackerApplicationTests {
 
 	@Test
