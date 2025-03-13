ba93e53a9b9bd84ee99ea8c84273f1a7b7f40c0c
Swarnim Shukla <swarnim0300@gmail.com>
2025-03-14 02:44:54 +0530

    feature: dockerize

--- src/main/java/com/bank/custaccttracker/CustacctrackerApplication.java
+++ src/main/java/com/bank/custaccttracker/CustacctrackerApplication.java
@@ -2,11 +2,12 @@ package com.bank.custaccttracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
 @SpringBootApplication
-public class CustacctrackerApplication {
+public class CustacctrackerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustacctrackerApplication.class, args);
